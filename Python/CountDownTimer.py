6bb8c448f57d3888a2d4da18f343a45f90088332
Siddharth Andhale <sidd1212004@gmail.com>
2022-10-10 23:23:19 +0530

    Added CountDownTimer

---
+++ Python/CountDownTimer.py
@@ -0,0 +1,14 @@
+import time
+
+def countdown(time_sec):
+    while time_sec:
+        mins, secs = divmod(time_sec, 60)
+        timeformat = '{:02d}:{:02d}'.format(mins, secs)
+        print(timeformat, end='\r')
+        time.sleep(1)
+        time_sec -= 1
+
+    print(" Time Up! ")
+
+sec = int(input("Enter Countdown Time (in sec) :- "))
+countdown(sec)
